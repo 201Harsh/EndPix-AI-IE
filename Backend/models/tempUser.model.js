21611207c9789731dc50b1c2140018ae2a106e96
Harsh <gamerpandeyharsh@gmail.com>
2025-07-01 10:46:23 +0530

    Basic Folder Setup Backend

---
+++ Backend/models/tempUser.model.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const tempUserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  otp: {
+    type: String,
+    required: true,
+  },
+  otpExpiry: {
+    type: Date,
+    required: true,
+  },
+});
+
+tempUserSchema.index({ otpExpiry: 1 }, { expireAfterSeconds: 300 });
+
+
+const TempUser = mongoose.model("TempUser", tempUserSchema);
+
+module.exports = TempUser;
